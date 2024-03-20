b81f198879ef9d8c847657e16bae4e62e3d2039b
Cassio Manuguerra <91049597+CassioManuguerra@users.noreply.github.com>
2024-03-20 15:41:36 +0100

    configure sonar

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,15 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.androidApplication) apply false
-    alias(libs.plugins.jetbrainsKotlinAndroid) apply false
+    id("org.sonarqube") version "4.4.1.3373"
+    id("com.android.application") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+    id("com.google.gms.google-services") version "4.4.0" apply false
+}
+
+sonar {
+  properties {
+    property("sonar.projectKey", "LASTA-OUTDOOR_lasta")
+    property("sonar.organization", "lasta")
+    property("sonar.host.url", "https://sonarcloud.io")
+  }
 }
