570567fde3f02b290fc808924420c263e31e52d3
Andrew Limond <andrew.limond@epfl.ch>
2024-03-25 22:39:26 +0100

    Changed Gradle version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("org.sonarqube") version "4.4.1.3373"
-    id("com.android.application") version "8.2.2" apply false
+    id("com.android.application") version "8.3.1" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
     id("com.google.gms.google-services") version "4.4.0" apply false
 }
