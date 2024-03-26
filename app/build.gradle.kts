6df3b5b29740d49082e557653567685dffb926fd
Cassio Manuguerra <91049597+CassioManuguerra@users.noreply.github.com>
2024-03-26 17:02:46 +0100

    Fixed json with all sha-1 and fixed sonar in app build.gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -94,9 +94,9 @@ android {
 
 sonar {
     properties {
-        property("sonar.projectKey", "gf_android-sample")
-        property("sonar.projectName", "Android-Sample")
-        property("sonar.organization", "gabrielfleischer")
+        property("sonar.projectKey", "LASTA-OUTDOOR_lasta")
+        //property("sonar.projectName", "Lasta")
+        property("sonar.organization", "lasta")
         property("sonar.host.url", "https://sonarcloud.io")
         // Comma-separated paths to the various directories containing the *.xml JUnit report files. Each path may be absolute or relative to the project base directory.
         property("sonar.junit.reportPaths", "${project.layout.buildDirectory.get()}/test-results/testDebugunitTest/")
