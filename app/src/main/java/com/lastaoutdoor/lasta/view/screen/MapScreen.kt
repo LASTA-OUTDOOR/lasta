7e9ce2253255c8172056ac001b533ee3077fd042
Jérémy Hugentobler <jeremy.hugentobler@epfl.ch>
2024-03-29 12:59:30 +0100

    Map with icon and uses the dataclass for activities
    I spent a lot of time trying to implement live position in the map but it doesn't work for now.
    
    I created a data class for the activities on the map and you can see an example on the map in lausanne.
    
    This part is not following an MVVM architecture for now and this will be changed in the following days.

--- app/src/main/java/com/lastaoutdoor/lasta/view/screen/MapScreen.kt
+++ app/src/main/java/com/lastaoutdoor/lasta/view/screen/MapScreen.kt
@@ -1,36 +1,53 @@
 package com.lastaoutdoor.lasta.view.screen
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.firebase.ui.auth.AuthUI.getApplicationContext
+import com.google.android.gms.maps.MapsInitializer
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
+import com.lastaoutdoor.lasta.R
+import com.lastaoutdoor.lasta.model.data.activityMarker
+
+// This composable will be used to manage the permissions for the map
 
 // This composable shows a map provided by Google Maps and will be used to show the activities
 // locations
+@SuppressLint("RestrictedApi")
 @Composable
 fun MapScreen() {
 
-  // Initial values for testing the map implementation (will not stay once we can fetch activities)
-  val lausanne = LatLng(46.519962, 6.633597)
-  val zoom = 10f
+  // Initialise the map, otherwise the icon functionnality won't work
+  MapsInitializer.initialize(getApplicationContext())
 
-  // The configuration part took some time, go to github wiki for more information
+  // Initial values for testing the map implementation (will not stay once we can fetch activities)
+  val lausanne =
+      activityMarker(
+          "Lausanne",
+          LatLng(46.519962, 6.633597),
+          "Example activity in Lausanne",
+          "Lausanne",
+          BitmapDescriptorFactory.fromResource(R.drawable.discover_icon))
+  val zoom = 10f
 
   // camera that goes to the initial position and can be moved by the user
   val cameraPositionState = rememberCameraPositionState {
-    position = CameraPosition.fromLatLngZoom(lausanne, zoom)
+    position = CameraPosition.fromLatLngZoom(lausanne.position, zoom)
   }
 
   GoogleMap(modifier = Modifier.fillMaxSize(), cameraPositionState = cameraPositionState) {
     // Point on the map, we could have added multiple and changed more properties
     Marker(
-        state = MarkerState(position = lausanne),
-        title = "Lausanne",
-        snippet = "Test marker in Lausanne")
+        state = MarkerState(position = lausanne.position),
+        title = lausanne.name,
+        icon = lausanne.icon,
+        snippet = lausanne.description)
   }
 }
---
+++ app/src/main/java/com/lastaoutdoor/lasta/viewmodel/MapViewModel.kt
@@ -0,0 +1,3 @@
+package com.lastaoutdoor.lasta.viewmodel
+
+// TODO: Expand to a mvvm architecture
