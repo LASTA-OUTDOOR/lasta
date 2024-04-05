9d42888596843d0009a02050caccfac0dc16951b
JoniDutoit <95919886+JoniDutoit@users.noreply.github.com>
2024-04-05 11:49:17 +0200

    "debugging is boring"

--- app/src/main/java/com/lastaoutdoor/lasta/viewmodel/RecentActivitiesViewModel.kt
+++ app/src/main/java/com/lastaoutdoor/lasta/viewmodel/RecentActivitiesViewModel.kt
@@ -2,7 +2,8 @@ package com.lastaoutdoor.lasta.viewmodel
 
 import androidx.lifecycle.ViewModel
 import com.lastaoutdoor.lasta.repositories.ActivitiesRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
-
+@HiltViewModel
 class RecentActivitiesViewModel @Inject constructor(private val repository: ActivitiesRepository) :
     ViewModel() {}
