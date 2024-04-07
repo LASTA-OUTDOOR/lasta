c4904a50ed5256f23f4edccd1fe3b07fce2a930e
Andrew Limond <andrew.limond@epfl.ch>
2024-04-07 18:05:33 +0200

    Finished documenting, added some todo's

--- app/src/main/java/com/lastaoutdoor/lasta/di/AppModule.kt
+++ app/src/main/java/com/lastaoutdoor/lasta/di/AppModule.kt
@@ -10,14 +10,17 @@ import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
+/** Hilt Module for providing dependencies */
 @InstallIn(SingletonComponent::class)
 @Module
 object AppModule {
 
+  /** Provides the [GoogleAuth] class */
   @Singleton
   @Provides
   fun provideGoogleAuth(@ApplicationContext context: Context) = GoogleAuth(context)
 
+  /** Provides the [PreferencesDataStore] class */
   @Singleton
   @Provides
   fun providePreferencesDataStore(@ApplicationContext context: Context) =
--- app/src/main/java/com/lastaoutdoor/lasta/viewmodel/AuthViewModel.kt
+++ app/src/main/java/com/lastaoutdoor/lasta/viewmodel/AuthViewModel.kt
@@ -13,12 +13,23 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
+/**
+ * ViewModel class to handle authentication related operations
+ *
+ * @param googleAuth GoogleAuth instance to handle Google authentication
+ * @return [HiltViewModel] instance
+ */
 @HiltViewModel
 class AuthViewModel @Inject constructor(private val googleAuth: GoogleAuth) : ViewModel() {
 
+  // State flow instance set to signed out by default
   private val _authStateFlow: MutableStateFlow<AuthState> = MutableStateFlow(AuthState.SignedOut)
   val authStateFlow: StateFlow<AuthState> = _authStateFlow
 
+  /**
+   * Starts the Google sign-in process and if successful, updates the authStateFlow to
+   * [AuthState.GoogleSignInIntent] with the intent sender
+   */
   fun startGoogleSignIn() {
     viewModelScope.launch {
       try {
@@ -30,6 +41,13 @@ class AuthViewModel @Inject constructor(private val googleAuth: GoogleAuth) : Vi
     }
   }
 
+  /**
+   * Handles the result of the Google sign-in process from an intent If the sign-in is successful,
+   * updates the authStateFlow to [AuthState.Authenticated] with the signed-in user info. If the
+   * sign-in fails, updates the authStateFlow to [AuthState.Error] with the error message
+   *
+   * @param intent the intent containing the result of the sign-in process
+   */
   fun handleGoogleSignInResult(intent: Intent) {
     viewModelScope.launch {
       try {
@@ -47,6 +65,10 @@ class AuthViewModel @Inject constructor(private val googleAuth: GoogleAuth) : Vi
     }
   }
 
+  /**
+   * Signs out the current user and updates the authStateFlow to [AuthState.SignedOut] If the
+   * sign-out fails, updates the authStateFlow to [AuthState.Error] with the error message
+   */
   fun signOut() {
     viewModelScope.launch {
       try {
@@ -58,6 +80,11 @@ class AuthViewModel @Inject constructor(private val googleAuth: GoogleAuth) : Vi
     }
   }
 
+  /**
+   * Tries to get current auth instance from Firebase and updates the authStateFlow to
+   * [AuthState.Authenticated] with the user info if the user is signed in. Otherwise, updates the
+   * authStateFlow to [AuthState.SignedOut]
+   */
   fun fetchAuthInfo() {
     viewModelScope.launch {
       try {
@@ -73,22 +100,50 @@ class AuthViewModel @Inject constructor(private val googleAuth: GoogleAuth) : Vi
     }
   }
 
+  /**
+   * Helper method to check if the current state is [AuthState.GoogleSignInIntent] and if so,
+   * returns the [IntentSender] from the authStateFlow
+   *
+   * @return the IntentSender instance if the state is [AuthState.GoogleSignInIntent], otherwise
+   *   null
+   */
   fun getIntentSender(): IntentSender? {
     return (authStateFlow.value as? AuthState.GoogleSignInIntent)?.intentSender
   }
 
+  /**
+   * Helper method to get the current user info if the state is [AuthState.Authenticated]
+   *
+   * @return the [UserModel] instance if the state is [AuthState.Authenticated], otherwise null
+   */
   fun getCurrentUser(): UserModel? {
     return (authStateFlow.value as? AuthState.Authenticated)?.user
   }
 
-  // Enum class to represent the authentication state
+  /** Enum class to represent the different authentication states */
   sealed class AuthState {
+    /**
+     * Represents the state where a google sign-in intent has been created
+     *
+     * @param intentSender the [IntentSender] instance to start the sign-in process
+     */
     data class GoogleSignInIntent(val intentSender: IntentSender?) : AuthState()
 
+    /**
+     * Represents the state where a user has been authenticated and has valid user info
+     *
+     * @param user the [UserModel] instance representing the authenticated user
+     */
     data class Authenticated(val user: UserModel) : AuthState()
 
+    /** Represents the state where the user is/has signed out */
     object SignedOut : AuthState()
 
+    /**
+     * Represents the auth state where an error occurred
+     *
+     * @param errorMessage the error message
+     */
     data class Error(val errorMessage: String) : AuthState()
   }
 }
