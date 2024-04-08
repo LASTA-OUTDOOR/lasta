0bfd6bbe5028fcec82a0d741c9585d54f9a0b705
Jérémy Hugentobler <jeremy.hugentobler@epfl.ch>
2024-04-08 16:16:31 +0200

    Forgot KTFMT, pushed again to see the CI in action

--- app/src/test/java/com/lastaoutdoor/lasta/viewmodel/MapViewModelTest.kt
+++ app/src/test/java/com/lastaoutdoor/lasta/viewmodel/MapViewModelTest.kt
@@ -1,18 +1,15 @@
 package com.lastaoutdoor.lasta.viewmodel
 
 import org.junit.Assert.*
-
 import org.junit.Before
 import org.junit.Test
 
 class MapViewModelTest {
 
-    @Before
-    fun setUp() {
-    }
+  @Before fun setUp() {}
 
-    @Test
-    fun getState() {
-        assert(2+2==4)
-    }
+  @Test
+  fun getState() {
+    assert(2 + 2 == 4)
+  }
 }
