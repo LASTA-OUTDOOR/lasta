cc2cabaf6f1029cb3a299ee1af0b5a51e71db20c
Jérémy Hugentobler <jeremy.hugentobler@epfl.ch>
2024-04-08 15:44:02 +0200

    added some commentary, now trying to make the CI Work -> edited the ci.yml to be triggered on this branch (will remove on pr)

--- app/src/main/java/com/lastaoutdoor/lasta/data/model/map/MapItinerary.kt
+++ app/src/main/java/com/lastaoutdoor/lasta/data/model/map/MapItinerary.kt
@@ -2,8 +2,4 @@ package com.lastaoutdoor.lasta.data.model.map
 
 import com.google.android.gms.maps.model.LatLng
 
-data class MapItinerary(
-    val id: Long,
-    val name: String,
-    val points: List<LatLng>
-)
+data class MapItinerary(val id: Long, val name: String, val points: List<LatLng>)
