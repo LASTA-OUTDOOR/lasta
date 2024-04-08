cb4f2819f049f00783b5b12868cab72da51d11a4
Jérémy Hugentobler <jeremy.hugentobler@epfl.ch>
2024-04-08 16:12:34 +0200

    added a test case to debug the CI

--- app/src/test/java/com/lastaoutdoor/lasta/viewmodel/MapViewModelTest.kt
+++ app/src/test/java/com/lastaoutdoor/lasta/viewmodel/MapViewModelTest.kt
@@ -1,15 +1,18 @@
 package com.lastaoutdoor.lasta.viewmodel
 
 import org.junit.Assert.*
+
 import org.junit.Before
 import org.junit.Test
 
 class MapViewModelTest {
 
-  @Before fun setUp() {}
+    @Before
+    fun setUp() {
+    }
 
-  @Test
-  fun getState() {
-    assert(2 + 2 == 4)
-  }
+    @Test
+    fun getState() {
+        assert(2+2==4)
+    }
 }
