82bd8eb49c073e05359f9eadbbc9c32945bcd242
Jérémy Hugentobler <jeremy.hugentobler@epfl.ch>
2024-04-08 16:12:34 +0200

    added a test case to debug the CI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -133,6 +133,8 @@ dependencies {
     implementation(libs.androidx.room.ktx)
     implementation(libs.androidx.material)
     testImplementation(libs.junit)
+    testImplementation("junit:junit:4.12")
+    testImplementation("junit:junit:4.12")
     globalTestImplementation(libs.androidx.junit)
     globalTestImplementation(libs.androidx.espresso.core)
 
