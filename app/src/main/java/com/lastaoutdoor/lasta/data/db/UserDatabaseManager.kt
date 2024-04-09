3baa38ab6fa83aadeb92279ebbb16b331774d2fb
Thimphou <91149580+Thimphou@users.noreply.github.com>
2024-04-09 20:16:57 +0200

    Added functions to get data from activities database

--- app/src/main/java/com/lastaoutdoor/lasta/data/db/UserDatabaseManager.kt
+++ app/src/main/java/com/lastaoutdoor/lasta/data/db/UserDatabaseManager.kt
@@ -1,22 +1,28 @@
 package com.lastaoutdoor.lasta.data.db
 
 import com.google.firebase.Firebase
-import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.firestore.SetOptions
 import com.google.firebase.firestore.firestore
 import com.lastaoutdoor.lasta.data.model.UserModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.tasks.await
+import java.util.Date
+
+private const val USERS_COLLECTION = "users"
+private const val ACTIVITIES_COLLECTION = "activities_database"
+private const val ERR_NOT_FOUND = -1.0;
 
 /**
  * Class containing functions for interacting with the Firestore database
- *
- * TODO: Rename this class to be more specific to the data it is handling
- * TODO: Make user model interact with the database otherwise no consistency
  */
-class UserDatabaseManager {
+
+class DatabaseManager {
 
   // Attributes
   private val database = Firebase.firestore
+  private val activityConverter = ActivityConverter()
 
   /**
    * Function to add a user to the Firestore database
@@ -26,23 +32,33 @@ class UserDatabaseManager {
    */
   fun addUserToDatabase(user: UserModel) {
 
-    val userDocumentRef = database.collection("users").document(user.userId)
+    val userDocumentRef = database.collection(USERS_COLLECTION).document(user.userId)
 
     // Create a data map with the user's information
     val userData =
-        hashMapOf(
-            "email" to user.email,
-            "displayName" to user.userName,
-            "profilePictureUrl" to user.profilePictureUrl
-            // Add any other user information you want to store here
-            )
+      hashMapOf(
+        "uniqueId" to user.userId,
+        "email" to user.email,
+        "displayName" to user.userName,
+        "profilePictureUrl" to user.profilePictureUrl
+        // Add any other user information you want to store here
+      )
 
     userDocumentRef.set(userData, SetOptions.merge())
   }
 
+  /**
+   * Function to get a field from the user's document in the Firestore database
+   *
+   * @param uid The unique identifier of the user
+   * @param field The field to get
+   *
+   * @return The value of the field
+   *
+   */
   suspend fun getFieldFromUser(uid: String, field: String): String {
     // Create a reference to the document with the user's UID
-    val userDocumentRef = database.collection("users").document(uid)
+    val userDocumentRef = database.collection(USERS_COLLECTION).document(uid)
 
     // Get the document snapshot
     val documentSnapshot = userDocumentRef.get().await()
@@ -51,9 +67,17 @@ class UserDatabaseManager {
     return documentSnapshot.getString(field) ?: ""
   }
 
+  /**
+   * Function to add a field to the user's document in the Firestore database
+   *
+   * @param uid The unique identifier of the user
+   * @param field The field to add
+   * @param value The value of the field
+   *
+   */
   fun addFieldToUser(uid: String, field: String, value: String) {
     // Create a reference to the document with the user's UID
-    val userDocumentRef = database.collection("users").document(uid)
+    val userDocumentRef = database.collection(USERS_COLLECTION).document(uid)
 
     // Create a data map with the field and value
     val data = hashMapOf(field to value)
@@ -62,9 +86,18 @@ class UserDatabaseManager {
     userDocumentRef.set(data, SetOptions.merge())
   }
 
+  /**
+   * Function to update a field in the user's document in the Firestore database
+   *
+   * @param uid The unique identifier of the user
+   * @param field The field to update
+   * @param value The new value of the field
+   *
+   */
+
   fun updateFieldInUser(uid: String, field: String, value: String) {
     // Create a reference to the document with the user's UID
-    val userDocumentRef = database.collection("users").document(uid)
+    val userDocumentRef = database.collection(USERS_COLLECTION).document(uid)
 
     // Create a data map with the field and value
     val data = hashMapOf(field to value)
@@ -73,9 +106,82 @@ class UserDatabaseManager {
     userDocumentRef.update(data as Map<String, Any>)
   }
 
+  /**
+   * Function to create a new collection in the Firestore database
+   *
+   * @param collectionName The name of the collection to create
+   *
+   */
   fun createCollection(collectionName: String) {
     database.collection(collectionName)
   }
 
-  fun
+  /**
+   * Function to get a field of a hiking activity from the user's document in the Firestore database
+   *
+   * @param user The user to get the field from
+   * @param activityId The ID of the activity
+   * @param field The field to get
+   *
+   * @return The value of the field
+   *
+   */
+  suspend fun getFieldOfHiking(user: UserModel, activityId: Long, field: String): Any {
+    val userDocumentRef = database.collection(ACTIVITIES_COLLECTION).document(user.userId)
+    val document = userDocumentRef.get().await()
+    if (document != null) {
+      val hikingArray = document.get("Hiking") as? List<*>
+      if (hikingArray != null) {
+        for (item in hikingArray) {
+          val trail = activityConverter.databaseToTrail(item as HashMap<String, Any>)
+          if (trail.activityID == activityId) {
+            when (field) {
+              "avgSpeedInKMH" -> return trail.avgSpeedInKMH
+              "caloriesBurned" -> return trail.caloriesBurned.toDouble()
+              "distanceInMeters" -> return trail.distanceInMeters.toDouble()
+              "elevationChangeInMeters" -> return trail.elevationChangeInMeters.toDouble()
+              "timeStarted" -> return trail.timeStarted
+              "timeFinished" -> return trail.timeFinished
+            }
+          }
+        }
+      }
+    }
+    return ERR_NOT_FOUND
+  }
+
+  /**
+   * Function to set a hiking field from the user's document in the Firestore database
+   *
+   * @param uid The unique identifier of the user
+   * @param field The field to set
+   *
+   */
+
+  fun setFieldOfHiking(user: UserModel, activityId: Long, field: String, value: Any) {
+    val userDocumentRef = database.collection(ACTIVITIES_COLLECTION).document(user.userId)
+    CoroutineScope(Dispatchers.IO).launch {
+      val documentSnapshot = userDocumentRef.get().await()
+      if (documentSnapshot != null) {
+        val hikingArray = documentSnapshot.get("Hiking") as? List<*>
+        if (hikingArray != null) {
+          for (item in hikingArray) {
+            val trail = activityConverter.databaseToTrail(item as HashMap<String, Any>)
+            if (trail.activityID == activityId) {
+              when (field) {
+                "avgSpeedInKMH" -> trail.avgSpeedInKMH = value as Double
+                "caloriesBurned" -> trail.caloriesBurned = value as Long
+                "distanceInMeters" -> trail.distanceInMeters = value as Long
+                "elevationChangeInMeters" -> trail.elevationChangeInMeters = value as Long
+                "timeStarted" -> trail.timeStarted = value as Date
+                "timeFinished" -> trail.timeFinished = value as Date
+              }
+            }
+          }
+          userDocumentRef.update("Hiking", hikingArray)
+        }
+      }
+    }
+  }
 }
+
