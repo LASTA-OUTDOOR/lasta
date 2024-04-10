df824d7be7b4739ee0b03188284a668ee4e67b7c
Andrew Limond <andrew.limond@epfl.ch>
2024-04-10 18:00:27 +0200

    Trying some stuff

---
+++ app/src/test/java/com/lastaoutdoor/lasta/data/auth/AuthRepositoryImplTest.kt
@@ -0,0 +1,33 @@
+package com.lastaoutdoor.lasta.data.auth
+
+import org.junit.Assert.*
+
+import org.junit.Before
+import org.junit.Test
+
+class AuthRepositoryImplTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @Test
+    fun isUserAuthentificated() {
+    }
+
+    @Test
+    fun getCurrentUser() {
+    }
+
+    @Test
+    fun startGoogleSignIn() {
+    }
+
+    @Test
+    fun finishGoogleSignIn() {
+    }
+
+    @Test
+    fun signOut() {
+    }
+}
