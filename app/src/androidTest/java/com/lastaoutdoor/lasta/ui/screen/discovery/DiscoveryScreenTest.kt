d92f6305b17e310a0f7acfd0ae9ca0f866bc00a3
Andrew Limond <andrew.limond@epfl.ch>
2024-04-11 22:37:24 +0200

    Some changes

--- app/src/androidTest/java/com/lastaoutdoor/lasta/ui/screen/discovery/DiscoveryScreenTest.kt
+++ app/src/androidTest/java/com/lastaoutdoor/lasta/ui/screen/discovery/DiscoveryScreenTest.kt
@@ -8,12 +8,11 @@ import dagger.hilt.android.testing.HiltAndroidTest
 import dagger.hilt.android.testing.UninstallModules
 import org.junit.Rule
 
-@HiltAndroidTest @UninstallModules(AppModule::class) class DiscoveryScreenTest {
+@HiltAndroidTest
+@UninstallModules(AppModule::class)
+class DiscoveryScreenTest {
 
-    @get:Rule(order = 0)
-    val hiltRule = HiltAndroidRule(this)
+  @get:Rule(order = 0) val hiltRule = HiltAndroidRule(this)
 
-    @get:Rule(order = 1)
-    val composeRule = createAndroidComposeRule<MainActivity>()
-
+  @get:Rule(order = 1) val composeRule = createAndroidComposeRule<MainActivity>()
 }
