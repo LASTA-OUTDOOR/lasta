0e4392e9665f549a946d0d99a0fecc4d592e2bc4
Jérémy Hugentobler <jeremy.hugentobler@epfl.ch>
2024-04-11 11:34:20 +0200

    Mock for the repo

--- app/src/test/java/com/lastaoutdoor/lasta/viewmodel/MapViewModelTest.kt
+++ app/src/test/java/com/lastaoutdoor/lasta/viewmodel/MapViewModelTest.kt
@@ -1,12 +1,59 @@
 package com.lastaoutdoor.lasta.viewmodel
 
+import com.lastaoutdoor.lasta.data.api.OutdoorActivityResponse
+import com.lastaoutdoor.lasta.data.model.api.Node
+import com.lastaoutdoor.lasta.data.model.api.Relation
+import com.lastaoutdoor.lasta.data.model.api.Way
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
+import com.lastaoutdoor.lasta.repository.OutdoorActivityRepository
+
+//Class used to simulate the data given to the view model
+class MockRepository : OutdoorActivityRepository {
+  override fun getClimbingActivitiesNode(
+    range: Int,
+    lat: Double,
+    lon: Double
+  ): OutdoorActivityResponse<Node> {
+    TODO("Not yet implemented")
+  }
+
+  override fun getClimbingActivitiesWay(
+    range: Int,
+    lat: Double,
+    lon: Double
+  ): OutdoorActivityResponse<Way> {
+    TODO("Not yet implemented")
+  }
+
+  override fun getHikingActivities(
+    range: Int,
+    lat: Double,
+    lon: Double
+  ): OutdoorActivityResponse<Relation> {
+    TODO("Not yet implemented")
+  }
+
+  override fun getDataStringClimbing(range: Int, lat: Double, lon: Double, type: String): String {
+    //don't need to implement this function here
+    return ""
+  }
+
+  override fun getDataStringHiking(range: Int, lat: Double, lon: Double): String {
+    //don't need to implement this function here
+    return ""
+  }
+
+}
 
 class MapViewModelTest {
 
-  @Before fun setUp() {}
+  val repository = MockRepository()
+  val viewModel : MapViewModel = MapViewModel(repository)
+  @Before fun setUp() {
+
+  }
 
   @Test
   fun getState() {
