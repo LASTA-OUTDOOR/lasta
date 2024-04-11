7dba2b8e4e8eb57d7b6996016c39bd7065007058
Cassio Manuguerra <91049597+CassioManuguerra@users.noreply.github.com>
2024-04-11 21:07:54 +0200

    added rules

--- app/src/androidTest/java/com/lastaoutdoor/lasta/ui/screen/discovery/DiscoveryScreenTest.kt
+++ app/src/androidTest/java/com/lastaoutdoor/lasta/ui/screen/discovery/DiscoveryScreenTest.kt
@@ -1,7 +1,19 @@
 package com.lastaoutdoor.lasta.ui.screen.discovery
 
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import com.lastaoutdoor.lasta.di.AppModule
+import com.lastaoutdoor.lasta.ui.MainActivity
+import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import dagger.hilt.android.testing.UninstallModules
+import org.junit.Rule
 
-@HiltAndroidTest @UninstallModules(AppModule::class) class DiscoveryScreenTest {}
+@HiltAndroidTest @UninstallModules(AppModule::class) class DiscoveryScreenTest {
+
+    @get:Rule(order = 0)
+    val hiltRule = HiltAndroidRule(this)
+
+    @get:Rule(order = 1)
+    val composeRule = createAndroidComposeRule<MainActivity>()
+
+}
