34ab33bc41bc77ca1bdf3ee5a51e932511bc4237
Emile Cornamusaz <emile.cornamusaz@epfl.ch>
2024-04-11 17:04:27 +0200

    whole lot more testing

---
+++ app/src/test/java/com/lastaoutdoor/lasta/data/db/DatabasemanagerTest.kt
@@ -0,0 +1,14 @@
+package com.lastaoutdoor.lasta.data.db
+
+import org.junit.Before
+
+class DatabasemanagerTest {
+    private lateinit var databaseManager: DatabaseManager
+
+    @Before
+    fun setUp(){
+        databaseManager=DatabaseManager()
+    }
+
+
+}
--- app/src/test/java/com/lastaoutdoor/lasta/data/model/profile/TimeFrameTest.kt
+++ app/src/test/java/com/lastaoutdoor/lasta/data/model/profile/TimeFrameTest.kt
@@ -15,8 +15,39 @@ class TimeFrameTest {
 
     @Test
     fun `The toString() from MonthsInYear enum returns the right value`() {
-        assertEquals("J",MonthsInYear.Jul.toString())
+        assertEquals("J",MonthsInYear.Jan.toString())
+        assertEquals("F",MonthsInYear.Feb.toString())
         assertEquals("M",MonthsInYear.Mar.toString())
+        assertEquals("A",MonthsInYear.Apr.toString())
+        assertEquals("M",MonthsInYear.May.toString())
+        assertEquals("J",MonthsInYear.Jun.toString())
+        assertEquals("J",MonthsInYear.Jul.toString())
+        assertEquals("A",MonthsInYear.Aug.toString())
+        assertEquals("S",MonthsInYear.Sep.toString())
+        assertEquals("O",MonthsInYear.Oct.toString())
+        assertEquals("N",MonthsInYear.Nov.toString())
+        assertEquals("D",MonthsInYear.Dec.toString())
+    }
+
+    @Test
+    fun `The toString() from DaysInWeek dataclass returns the right value`() {
+        assertEquals("Mon",DaysInWeek.Mon.toString())
+        assertEquals("Tue",DaysInWeek.Tue.toString())
+        assertEquals("Wed",DaysInWeek.Wed.toString())
+        assertEquals("Thu",DaysInWeek.Thu.toString())
+        assertEquals("Fri",DaysInWeek.Fri.toString())
+        assertEquals("Sat",DaysInWeek.Sat.toString())
+        assertEquals("Sun",DaysInWeek.Sun.toString())
+
+    }
+    @Test
+    fun `The toString() from WeeksInMonth dataclass returns the right value`() {
+        assertEquals("Week1",WeeksInMonth.Week1.toString())
+        assertEquals("Week2",WeeksInMonth.Week2.toString())
+        assertEquals("Week3",WeeksInMonth.Week3.toString())
+        assertEquals("Week4",WeeksInMonth.Week4.toString())
+        assertEquals("Week5",WeeksInMonth.Week5.toString())
+
     }
 
     @Test
