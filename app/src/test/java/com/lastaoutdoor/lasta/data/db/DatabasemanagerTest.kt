c53ec99c9505f930e57e67e957023da8ef7f5529
Thimphou <91149580+Thimphou@users.noreply.github.com>
2024-04-11 23:10:26 +0200

    DiscoveryScreenViewModel tested

--- app/src/test/java/com/lastaoutdoor/lasta/data/db/DatabasemanagerTest.kt
+++ app/src/test/java/com/lastaoutdoor/lasta/data/db/DatabasemanagerTest.kt
@@ -1,9 +1,6 @@
 package com.lastaoutdoor.lasta.data.db
 
 import org.junit.Before
-import org.junit.Test
-import org.robolectric.annotation.Config
-
 
 class DatabasemanagerTest {
   private lateinit var databaseManager: DatabaseManager
