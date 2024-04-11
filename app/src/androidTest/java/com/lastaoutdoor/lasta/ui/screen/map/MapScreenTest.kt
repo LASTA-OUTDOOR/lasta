e688a2ebce1a587d09d80e46051e8c593d39ae50
Cassio Manuguerra <91049597+CassioManuguerra@users.noreply.github.com>
2024-04-11 23:17:44 +0200

    ktfmt -- PR coming to start a new branch

--- app/src/androidTest/java/com/lastaoutdoor/lasta/ui/screen/map/MapScreenTest.kt
+++ app/src/androidTest/java/com/lastaoutdoor/lasta/ui/screen/map/MapScreenTest.kt
@@ -1,20 +1,15 @@
 package com.lastaoutdoor.lasta.ui.screen.map
 
 import androidx.activity.compose.setContent
-import androidx.compose.material3.Text
 import androidx.compose.ui.test.assertIsDisplayed
-import androidx.compose.ui.test.assertIsNotDisplayed
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithTag
-import androidx.compose.ui.test.onNodeWithText
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import androidx.test.ext.junit.rules.ActivityScenarioRule
 import com.lastaoutdoor.lasta.di.AppModule
 import com.lastaoutdoor.lasta.ui.MainActivity
 import com.lastaoutdoor.lasta.ui.screen.discovery.DiscoveryScreen
-import com.lastaoutdoor.lasta.ui.screen.profile.ProfileScreen
 import com.lastaoutdoor.lasta.ui.theme.LastaTheme
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
@@ -28,30 +23,28 @@ import org.junit.Test
 @UninstallModules(AppModule::class)
 class MapScreenTest {
 
-    @get:Rule(order = 0) val hiltRule = HiltAndroidRule(this)
+  @get:Rule(order = 0) val hiltRule = HiltAndroidRule(this)
 
-    @get:Rule(order = 1) val composeRule = createAndroidComposeRule<MainActivity>()
+  @get:Rule(order = 1) val composeRule = createAndroidComposeRule<MainActivity>()
 
-    @Before
-    fun setUp() {
-        hiltRule.inject()
-        composeRule.activity.setContent {
-            val navController = androidx.navigation.compose.rememberNavController()
-            com.lastaoutdoor.lasta.ui.theme.LastaTheme {
-                androidx.navigation.compose.NavHost(
-                    navController = navController, startDestination = "DiscoveryScreen") {
-                    composable(route = "DiscoveryScreen") {
-                        com.lastaoutdoor.lasta.ui.screen.discovery.DiscoveryScreen()
-                    }
-                }
+  @Before
+  fun setUp() {
+    hiltRule.inject()
+    composeRule.activity.setContent {
+      val navController = androidx.navigation.compose.rememberNavController()
+      com.lastaoutdoor.lasta.ui.theme.LastaTheme {
+        androidx.navigation.compose.NavHost(
+            navController = navController, startDestination = "DiscoveryScreen") {
+              composable(route = "DiscoveryScreen") {
+                com.lastaoutdoor.lasta.ui.screen.discovery.DiscoveryScreen()
+              }
             }
-        }
+      }
     }
+  }
 
-    @Test
-    fun discoveryScreen_isDisplayed() {
-        composeRule.onNodeWithTag("Discovery").assertIsDisplayed()
-    }
-
-
+  @Test
+  fun discoveryScreen_isDisplayed() {
+    composeRule.onNodeWithTag("Discovery").assertIsDisplayed()
+  }
 }
