1922aae5994305c1bed5ac4f4282ede7e26bd776
Jérémy Hugentobler <jeremy.hugentobler@epfl.ch>
2024-04-17 22:18:54 +0200

    Still working on UI, it starts to look good !

--- app/src/main/java/com/lastaoutdoor/lasta/ui/screen/social/components/FriendsActivities.kt
+++ app/src/main/java/com/lastaoutdoor/lasta/ui/screen/social/components/FriendsActivities.kt
@@ -1,28 +1,38 @@
 package com.lastaoutdoor.lasta.ui.screen.social.components
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.room.util.TableInfo
 import com.lastaoutdoor.lasta.viewmodel.SocialViewModel
 
 @Composable
 fun FriendsActivityList(viewModel: SocialViewModel = hiltViewModel()) {
-  if (viewModel.latestFriendActivities.isNullOrEmpty()) {
+  if (!viewModel.latestFriendActivities.isNullOrEmpty()) {
     FriendsMissing()
     return
   }
@@ -36,14 +46,30 @@ fun FriendsActivityCard(i: Int) {
           CardDefaults.cardColors(
               containerColor = MaterialTheme.colorScheme.surfaceVariant,
           ),
-      modifier = Modifier.height(height = 100.dp).fillMaxWidth().padding(8.dp)) {
-        Row {
-          Icon(
-              Icons.Filled.AccountCircle,
-              contentDescription = "Profile picture",
-              modifier = Modifier.padding(8.dp).size(30.dp).align(Alignment.CenterVertically))
-          Text(text = "Friend $i", modifier = Modifier.align(Alignment.CenterVertically))
-        }
-        Text(text = "Hiked 200km and went for a MC Donalds", modifier = Modifier.padding(8.dp))
+      modifier = Modifier
+
+          .fillMaxWidth()
+          .padding(8.dp))
+  {
+      Row(modifier = Modifier
+          .fillMaxWidth()
+          .padding(8.dp)){
+          Column (modifier = Modifier.fillMaxWidth(0.7f)){
+              Text(text = "Activity title", style = TextStyle(fontSize = 20.sp, fontWeight = FontWeight.Bold))
+              Text(text = "Hiked 200km and went for a MC Donalds", modifier = Modifier.padding(8.dp))
+          }
+          Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.End) {
+              Text(text = "Friend $i", modifier = Modifier.align(Alignment.CenterVertically))
+              Icon(
+                  Icons.Filled.AccountCircle,
+                  contentDescription = "Profile picture",
+                  modifier = Modifier
+                      .size(30.dp)
+                      .align(Alignment.CenterVertically)
+              )
+          }
       }
+      HorizontalDivider(thickness = 1.dp, color = Color.Gray)
+      Text("Comments", modifier = Modifier.padding(8.dp))
+  }
 }
