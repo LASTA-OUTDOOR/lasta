bc5799f8c2af755b7880a485ad2883d5aa1facc3
Andrew Limond <andrew.limond@epfl.ch>
2024-04-26 13:28:39 +0200

    First commit of this refactor branch

--- app/src/main/java/com/lastaoutdoor/lasta/data/model/user/UserModel.kt
+++ app/src/main/java/com/lastaoutdoor/lasta/data/model/user/UserModel.kt
@@ -2,17 +2,6 @@ package com.lastaoutdoor.lasta.data.model.user
 
 import com.google.firebase.auth.FirebaseUser
 
-/**
- * Data class representing a user
- *
- * @property userId the unique identifier of the user
- * @property userName the name of the user
- * @property email the email of the user
- * @property profilePictureUrl the URL of the user's profile picture
- * @property bio the bio of the user
- *
- * TODO: Make all model files match with database so that updating and writing will be easier
- */
 data class UserModel(
     val userId: String,
     val userName: String?,
