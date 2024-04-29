7101fafcd727561a152a41803db6b628b81b77ce
Andrew Limond <andrew.limond@epfl.ch>
2024-04-29 15:31:19 +0200

    Finished Login Screen refactor

--- app/src/main/java/com/lastaoutdoor/lasta/ui/navigation/LoginNavGraph.kt
+++ app/src/main/java/com/lastaoutdoor/lasta/ui/navigation/LoginNavGraph.kt
@@ -1,18 +1,42 @@
 package com.lastaoutdoor.lasta.ui.navigation
 
+import androidx.compose.runtime.collectAsState
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.composable
 import androidx.navigation.navigation
+import com.lastaoutdoor.lasta.models.user.UserModel
 import com.lastaoutdoor.lasta.ui.screen.login.LoginScreen
 import com.lastaoutdoor.lasta.ui.screen.setup.SetupScreen
 import com.lastaoutdoor.lasta.viewmodel.AuthViewModel
+import com.lastaoutdoor.lasta.viewmodel.PreferencesViewModel
 
 fun NavGraphBuilder.addLoginNavGraph(navController: NavHostController) {
   navigation(startDestination = DestinationRoute.SignIn.route, route = BaseRoute.Login.route) {
     composable(DestinationRoute.SignIn.route) { entry ->
       val authViewModel: AuthViewModel = entry.sharedViewModel(navController)
-      LoginScreen()
+      val preferencesViewModel: PreferencesViewModel = entry.sharedViewModel(navController)
+      val isSignUp = authViewModel.isSignUp.collectAsState(initial = false).value
+      LoginScreen(
+          authViewModel.beginSignInResult,
+          authViewModel.user,
+          isSignUp,
+          authViewModel::startGoogleSignIn,
+          authViewModel::finishGoogleSignIn,
+          authViewModel.oneTapClient,
+          { user: UserModel ->
+            preferencesViewModel.updateIsLoggedIn(true)
+            preferencesViewModel.updateUserInfo(user)
+          },
+          {
+            navController.navigate(DestinationRoute.Setup.route) {
+              popUpTo(BaseRoute.Login.route) { inclusive = true }
+            }
+          },
+          {
+            navController.popBackStack()
+            navController.navigate(BaseRoute.Main.route)
+          })
     }
     composable(DestinationRoute.Setup.route) { SetupScreen() }
   }
--- app/src/main/java/com/lastaoutdoor/lasta/ui/screen/login/LoginScreen.kt
+++ app/src/main/java/com/lastaoutdoor/lasta/ui/screen/login/LoginScreen.kt
@@ -9,23 +9,27 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.rememberNavController
+import com.google.android.gms.auth.api.identity.BeginSignInResult
+import com.google.android.gms.auth.api.identity.SignInClient
 import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.AuthCredential
 import com.google.firebase.auth.GoogleAuthProvider
+import com.lastaoutdoor.lasta.models.user.UserModel
 import com.lastaoutdoor.lasta.ui.navigation.BaseRoute
 import com.lastaoutdoor.lasta.ui.navigation.DestinationRoute
 import com.lastaoutdoor.lasta.ui.screen.login.components.LoginContent
-import com.lastaoutdoor.lasta.viewmodel.AuthViewModel
-import com.lastaoutdoor.lasta.viewmodel.PreferencesViewModel
 
 @Composable
 fun LoginScreen(
-    rootNavController: NavHostController = rememberNavController(),
-    navController: NavHostController = rememberNavController(),
-    authViewModel: AuthViewModel = hiltViewModel(),
-    preferencesViewModel: PreferencesViewModel = hiltViewModel(),
+    beginSignInResult: BeginSignInResult?,
+    user: UserModel?,
+    isSignUp: Boolean,
+    startGoogleSignIn: () -> Unit,
+    finishGoogleSignIn: (AuthCredential) -> Unit,
+    oneTapClient: SignInClient,
+    updatePreferencesOnLogin: (UserModel) -> Unit,
+    navigateToSetup: () -> Unit,
+    navigateToMain: () -> Unit
 ) {
   Modifier.testTag("LoginScreen")
 
@@ -34,38 +38,34 @@ fun LoginScreen(
           result ->
         if (result.resultCode == Activity.RESULT_OK) {
           try {
-            val credentials = authViewModel.oneTapClient.getSignInCredentialFromIntent(result.data)
+            val credentials = oneTapClient.getSignInCredentialFromIntent(result.data)
             val googleIdToken = credentials.googleIdToken
             val googleCredentials = GoogleAuthProvider.getCredential(googleIdToken, null)
-            authViewModel.finishGoogleSignIn(googleCredentials)
+            finishGoogleSignIn(googleCredentials)
           } catch (it: ApiException) {
             print(it)
           }
         }
       }
 
-  LaunchedEffect(key1 = authViewModel.beginSignInResult) {
-    authViewModel.beginSignInResult?.let {
+  LaunchedEffect(key1 = beginSignInResult) {
+    beginSignInResult?.let {
       val intent = IntentSenderRequest.Builder(it.pendingIntent.intentSender).build()
       launcher.launch(intent)
     }
   }
 
-  LaunchedEffect(key1 = authViewModel.user) {
-    authViewModel.user?.let {
-      preferencesViewModel.updateIsLoggedIn(true)
-      preferencesViewModel.updateUserInfo(it)
-      if (authViewModel.isSignUp) {
-        navController.navigate(DestinationRoute.Setup.route) {
-          popUpTo(BaseRoute.Login.route) { inclusive = true }
-        }
+  LaunchedEffect(key1 = user) {
+    user?.let {
+      updatePreferencesOnLogin(it)
+      if (isSignUp) {
+        navigateToSetup()
       } else {
-        rootNavController.popBackStack()
-        rootNavController.navigate(BaseRoute.Main.route)
+        navigateToMain()
       }
     }
   }
   Spacer(modifier = Modifier.testTag("loginScreenMain"))
 
-  LoginContent(onLoginClick = { authViewModel.startGoogleSignIn() })
+  LoginContent(onLoginClick = { startGoogleSignIn() })
 }
