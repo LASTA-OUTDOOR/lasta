bbd63a63bb7a166a5cffe26ab3e2204813e98ad5
npfrei <126064815+npfrei@users.noreply.github.com>
2024-05-01 14:07:36 +0200

    added 1 test

--- app/src/androidTest/java/com/lastaoutdoor/lasta/ui/screen/map/MapScreenTest.kt
+++ app/src/androidTest/java/com/lastaoutdoor/lasta/ui/screen/map/MapScreenTest.kt
@@ -1,45 +1,61 @@
 package com.lastaoutdoor.lasta.ui.screen.map
 
+import androidx.activity.compose.setContent
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertIsNotDisplayed
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithTag
 import com.lastaoutdoor.lasta.di.AppModule
+import com.lastaoutdoor.lasta.ui.MainActivity
+import com.lastaoutdoor.lasta.viewmodel.MapState
+import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import dagger.hilt.android.testing.UninstallModules
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
 
-@HiltAndroidTest @UninstallModules(AppModule::class) class MapScreenTest {}
-/*
-@get:Rule(order = 0) val hiltRule = HiltAndroidRule(this)
+@HiltAndroidTest
+@UninstallModules(AppModule::class)
+class MapScreenTest {
 
-@get:Rule(order = 1) val composeRule = createAndroidComposeRule<MainActivity>()
+  @get:Rule(order = 0) val hiltRule = HiltAndroidRule(this)
 
-@Before
-fun setUp() {
-  hiltRule.inject()
-}
+  @get:Rule(order = 1) val composeRule = createAndroidComposeRule<MainActivity>()
 
-private var isSheetOpen by mutableStateOf(false)
-}
-
-// Test that the bottom sheet works as wanted
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Test
-fun bottomSheetTestInitial() {
-
-  composeRule.activity.setContent {
-    val sheetState = rememberModalBottomSheetState()
-    InformationSheet(
-        sheetState = sheetState,
-        isSheetOpen = isSheetOpen,
-        onDismissRequest = { isSheetOpen = false })
+  @Before
+  fun setUp() {
+    hiltRule.inject()
   }
 
-  composeRule.onNodeWithTag("bottomSheet").assertIsNotDisplayed()
-  isSheetOpen = true
-  composeRule.onNodeWithTag("bottomSheet").assertIsDisplayed()
-  isSheetOpen = false
+  private var isSheetOpen by mutableStateOf(false)
+
+  // Test that the bottom sheet works as wanted
+
+  @OptIn(ExperimentalMaterial3Api::class)
+  @Test
+  fun bottomSheetTestInitial() {
+
+    composeRule.activity.setContent {
+      val sheetState = rememberModalBottomSheetState()
+      InformationSheet(
+          sheetState = sheetState,
+          isSheetOpen = isSheetOpen,
+          MapState(),
+          onDismissRequest = { isSheetOpen = false })
+    }
+
+    composeRule.onNodeWithTag("bottomSheet").assertIsNotDisplayed()
+    isSheetOpen = true
+    composeRule.onNodeWithTag("bottomSheet").assertIsDisplayed()
+    isSheetOpen = false
+  }
 }
-*/
 
 /*
   @OptIn(ExperimentalMaterial3Api::class)
