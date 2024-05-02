5206861044abe0a24f30c7d78b22e56f872d677c
npfrei <126064815+npfrei@users.noreply.github.com>
2024-05-02 17:30:42 +0200

    COmplete testing Social masha'Allah (97%) 2

--- app/src/test/java/com/lastaoutdoor/lasta/data/db/ActivityRepositoryImplTest.kt
+++ app/src/test/java/com/lastaoutdoor/lasta/data/db/ActivityRepositoryImplTest.kt
@@ -1,61 +1,67 @@
+import android.app.Activity
+import com.google.android.gms.tasks.OnFailureListener
+import com.google.android.gms.tasks.OnSuccessListener
+import com.google.android.gms.tasks.Task
 import io.mockk.*
+import java.util.concurrent.Executor
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 
 @ExperimentalCoroutinesApi
 class UserActivitiesRepositoryImplTest {
-  /*public class mockTask<T : Any> : Task<T>() {
-      override fun addOnFailureListener(p0: OnFailureListener): Task<T> {
-        return this
-      }
-
-      override fun addOnFailureListener(p0: Activity, p1: OnFailureListener): Task<T> {
-        return this
-      }
-
-      override fun addOnFailureListener(p0: Executor, p1: OnFailureListener): Task<T> {
-        return this
-      }
-
-      override fun getException(): Exception? {
-        return null
-      }
-
-      override fun getResult(): T {
-        return this.getResult()
-      }
-
-      override fun <X : Throwable?> getResult(p0: Class<X>): T {
-        return this.getResult()
-      }
-
-      override fun isCanceled(): Boolean {
-        return false
-      }
-
-      override fun isComplete(): Boolean {
-        return true
-      }
-
-      override fun isSuccessful(): Boolean {
-        return true
-      }
-
-      override fun addOnSuccessListener(p0: Executor, p1: OnSuccessListener<in T>): Task<T> {
-        return this
-      }
-
-      override fun addOnSuccessListener(p0: Activity, p1: OnSuccessListener<in T>): Task<T> {
-        return this
-      }
-
-      override fun addOnSuccessListener(p0: OnSuccessListener<in T>): Task<T> {
-        return this
-      }
-
-      fun await(): T {
-        return getResult()
-      }
+  public class mockTask<T : Any> : Task<T>() {
+    override fun addOnFailureListener(p0: OnFailureListener): Task<T> {
+      return this
     }
+
+    override fun addOnFailureListener(p0: Activity, p1: OnFailureListener): Task<T> {
+      return this
+    }
+
+    override fun addOnFailureListener(p0: Executor, p1: OnFailureListener): Task<T> {
+      return this
+    }
+
+    override fun getException(): Exception? {
+      return null
+    }
+
+    override fun getResult(): T {
+      return this.getResult()
+    }
+
+    override fun <X : Throwable?> getResult(p0: Class<X>): T {
+      return this.getResult()
+    }
+
+    override fun isCanceled(): Boolean {
+      return false
+    }
+
+    override fun isComplete(): Boolean {
+      return true
+    }
+
+    override fun isSuccessful(): Boolean {
+      return true
+    }
+
+    override fun addOnSuccessListener(p0: Executor, p1: OnSuccessListener<in T>): Task<T> {
+      return this
+    }
+
+    override fun addOnSuccessListener(p0: Activity, p1: OnSuccessListener<in T>): Task<T> {
+      return this
+    }
+
+    override fun addOnSuccessListener(p0: OnSuccessListener<in T>): Task<T> {
+      return this
+    }
+
+    fun await(): T {
+      return getResult()
+    }
+  }
+  /*
 
     var expectedActivitiesHK = hashMapOf("Hiking" to arrayListOf<ActivitiesDatabaseType.Trail>())
     private val firestore: FirebaseFirestore = mockk(relaxed = true)
