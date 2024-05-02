6633a5e199c426bc37247a505a63810c5b6f5d37
Jonathan <jonathan.dutoit@epfl.ch>
2024-05-02 18:17:45 +0200

    "added a few tests"

--- app/src/test/java/com/lastaoutdoor/lasta/viewmodel/DiscoveryScreenViewModelTest.kt
+++ app/src/test/java/com/lastaoutdoor/lasta/viewmodel/DiscoveryScreenViewModelTest.kt
@@ -1,9 +1,14 @@
 package com.lastaoutdoor.lasta.viewmodel
 
 import com.google.android.gms.maps.model.LatLng
+import com.lastaoutdoor.lasta.R
+import com.lastaoutdoor.lasta.models.activity.ActivityType
+import com.lastaoutdoor.lasta.models.map.Marker
 import com.lastaoutdoor.lasta.repository.api.ActivityRepository
+import io.mockk.every
 import io.mockk.mockk
 import junit.framework.TestCase.assertEquals
+import kotlinx.coroutines.test.runTest
 import org.junit.Before
 import org.junit.Test
 
@@ -16,6 +21,7 @@ class DiscoveryScreenViewModelTest {
   fun setUp() {
     viewModel = DiscoverScreenViewModel(repository)
   }
+
 
   @Test
   fun testInitialValues() {
@@ -30,4 +36,31 @@ class DiscoveryScreenViewModelTest {
             "Payerne" to LatLng(46.834190, 6.928969),
             "Matterhorn" to LatLng(45.980537, 7.641618)))
   }
+
+    @Test
+    fun testPUpdatePermissions(){
+        viewModel.updatePermission(true)
+        assertEquals(viewModel.initialPosition, LatLng(46.519962, 6.633597))
+        viewModel.updatePermission(false)
+        assertEquals(viewModel.initialPosition, LatLng(46.519962, 6.633597))
+    }
+
+    @Test
+    fun testClearSelectedItinerary() {
+        viewModel.clearSelectedItinerary()
+        assertEquals(viewModel.selectedItinerary.value, null)
+    }
+
+    @Test
+    fun testClearSelectedMarker(){
+        viewModel.clearSelectedMarker()
+        assertEquals(viewModel.selectedMarker.value, null)
+    }
+
+    @Test
+    fun testUpdateMarkers(){
+            viewModel.updateMarkers(LatLng(46.519962, 6.633597), 10000.0)
+            assertEquals(viewModel.markerList.value, viewModel.markerList.value)
+    }
+
 }
