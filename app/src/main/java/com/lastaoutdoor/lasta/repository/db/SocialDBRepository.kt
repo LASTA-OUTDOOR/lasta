2b6a1ee9454e0ec94cd018174672f84935417103
Andrew Limond <andrew.limond@epfl.ch>
2024-05-02 18:10:30 +0200

    Documented and finished social and all DB functions

--- app/src/main/java/com/lastaoutdoor/lasta/repository/db/SocialDBRepository.kt
+++ app/src/main/java/com/lastaoutdoor/lasta/repository/db/SocialDBRepository.kt
@@ -4,36 +4,88 @@ import com.lastaoutdoor.lasta.models.social.ConversationModel
 import com.lastaoutdoor.lasta.models.user.UserActivity
 import com.lastaoutdoor.lasta.models.user.UserModel
 
+/**
+ * This interface provides methods for interacting with social data.
+ */
 interface SocialDBRepository {
 
-  // returns all the friends of the user
+  /**
+   * Returns the list of the user friends
+   *
+   * @param friendIds the IDs of the friends
+   * @return a list of user models representing all the friends
+   */
   suspend fun getFriends(friendIds: List<String>): List<UserModel>
 
+  /**
+   * Returns the list of the user friend requests
+   *
+   * @param userId the ID of the user
+   * @return a list of user models representing all the friend requests from the users
+   */
   suspend fun getFriendRequests(userId: String): List<UserModel>
 
-  // returns all the activities done by friends in the last
+  /**
+   * Gets all the friend activities of the user with the given ID.
+   *
+   * @param userId the ID of the user
+   * @param days the number of days to look back
+   */
   suspend fun getLatestFriendActivities(userId: String, days: Int): List<UserActivity>
 
-  // returns all the conversations of the user
 
+  /**
+   * Returns the conversation between the user with the given ID and the friend with the given ID.
+   *
+   * @param userId the ID of the user
+   * @param friendId the ID of the friend
+   * @param createNew whether to create a new conversation if it doesn't exist
+   */
   suspend fun getConversation(
       userId: String,
       friendId: String,
       createNew: Boolean = true
   ): ConversationModel
 
+  /**
+   * Returns all the conversations of the user with the given ID.
+   *
+   * @param userId the ID of the user
+   * @return a list of conversation models representing all the conversations
+   */
   suspend fun getAllConversations(userId: String): List<ConversationModel>
 
-  // sends a friend request to the user with the given email, returns true if the request was sent
-  // successfully
+  /**
+   * Sends a friend request from the user with the given ID to the receiver with the given ID.
+   *
+   * @param userId the ID of the user
+   * @param receiverId the ID of the receiver
+   * @return true if the friend request is sent successfully, false otherwise
+   */
   suspend fun sendFriendRequest(userId: String, receiverId: String): Boolean
 
-  // accept a friend request
+  /**
+   * Accepts a friend request from the source with the given ID to the requester with the given ID.
+   *
+   * @param source the ID of the source
+   * @param requester the ID of the requester
+   */
   suspend fun acceptFriendRequest(source: String, requester: String)
 
-  // decline a friend request
+  /**
+   * Declines a friend request from the source with the given ID to the requester with the given ID.
+   *
+   * @param source the ID of the source
+   * @param requester the ID of the requester
+   */
   suspend fun declineFriendRequest(source: String, requester: String)
 
-  // send a message from a user to a friend
+  /**
+   * Sends a message from the user with the given ID to the friend with the given ID.
+   *
+   * @param userId the ID of the user
+   * @param friendUserId the ID of the friend
+   * @param message the message to send
+   */
   suspend fun sendMessage(userId: String, friendUserId: String, message: String)
 }
--- app/src/main/java/com/lastaoutdoor/lasta/repository/db/UserActivitiesDBRepository.kt
+++ app/src/main/java/com/lastaoutdoor/lasta/repository/db/UserActivitiesDBRepository.kt
@@ -5,7 +5,19 @@ import com.lastaoutdoor.lasta.models.user.UserActivity
 /** This interface provides methods for interacting with user activities data. */
 interface UserActivitiesDBRepository {
 
+  /**
+   * Returns all the activities of the user with the given ID.
+   *
+   * @param userId the ID of the user
+   * @return a list of user activities
+   */
   suspend fun getUserActivities(userId: String): List<UserActivity>
 
+  /**
+   * Adds a new user activity to the user with the given ID.
+   *
+   * @param userId the ID of the user
+   * @param userActivity the user activity to add
+   */
   suspend fun addUserActivity(userId: String, userActivity: UserActivity)
 }
