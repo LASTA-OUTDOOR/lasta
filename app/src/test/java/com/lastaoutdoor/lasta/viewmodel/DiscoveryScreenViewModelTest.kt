1076c50439dfc035d61f11e0f95f4f14d60a47e2
Jonathan <jonathan.dutoit@epfl.ch>
2024-05-03 02:16:38 +0200

    More tests!!!

--- app/src/test/java/com/lastaoutdoor/lasta/viewmodel/DiscoveryScreenViewModelTest.kt
+++ app/src/test/java/com/lastaoutdoor/lasta/viewmodel/DiscoveryScreenViewModelTest.kt
@@ -1,19 +1,41 @@
 package com.lastaoutdoor.lasta.viewmodel
 
 import com.google.android.gms.maps.model.LatLng
+import com.lastaoutdoor.lasta.models.activity.ActivityType
+import com.lastaoutdoor.lasta.models.api.Relation
+import com.lastaoutdoor.lasta.models.api.Tags
+import com.lastaoutdoor.lasta.models.map.MapItinerary
+import com.lastaoutdoor.lasta.models.map.Marker
 import com.lastaoutdoor.lasta.repository.api.ActivityRepository
+import com.lastaoutdoor.lasta.utils.Response
+import io.mockk.coEvery
 import io.mockk.mockk
 import junit.framework.TestCase.assertEquals
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestDispatcher
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import kotlinx.coroutines.withContext
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
+import org.junit.rules.TestWatcher
+import org.junit.runner.Description
 
 class DiscoveryScreenViewModelTest {
 
+  @get:Rule val mainDispatcherRule = MainDispatcherRule()
+
   private lateinit var viewModel: DiscoverScreenViewModel
-  private val repository = mockk<ActivityRepository>()
+  private lateinit var repository: ActivityRepository
 
   @Before
   fun setUp() {
+    repository = mockk(relaxed = true)
     viewModel = DiscoverScreenViewModel(repository)
   }
 
@@ -55,5 +77,39 @@ class DiscoveryScreenViewModelTest {
   fun testUpdateMarkers() {
     viewModel.updateMarkers(LatLng(46.519962, 6.633597), 10000.0)
     assertEquals(viewModel.markerList.value, viewModel.markerList.value)
+  }
+
+  @OptIn(ExperimentalCoroutinesApi::class)
+  @Test
+  fun `updateSelectedMarker updates marker and triggers itinerary fetch`() = runTest {
+    withContext(Dispatchers.Main) {
+
+      // Prepare
+      val marker =
+          Marker(1L, "Trail Name", LatLng(45.0, -45.0), "Description", 0, ActivityType.HIKING)
+      val expectedRelation = Relation("Relationt Test", 1L, Tags(""), emptyList())
+      val expectedItinerary = MapItinerary(1L, "Trail Name", listOf(LatLng(45.0, -45.0)))
+
+      // Mock the repository response
+      coEvery { repository.getHikingRouteById(1L) } returns Response.Success(expectedRelation)
+
+      //
+      viewModel.updateSelectedMarker(marker)
+      // Wait for the coroutine to finish
+      advanceUntilIdle()
+      assertEquals(marker, viewModel.selectedMarker.value)
+    }
+  }
+}
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class MainDispatcherRule(private val testDispatcher: TestDispatcher = UnconfinedTestDispatcher()) :
+    TestWatcher() {
+  override fun starting(description: Description) {
+    Dispatchers.setMain(testDispatcher)
+  }
+
+  override fun finished(description: Description) {
+    Dispatchers.resetMain()
   }
 }
