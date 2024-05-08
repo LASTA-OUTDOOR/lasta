6c4304050f72434fe5699fffc5e6109b546c2b44
Cassio Manuguerra <91049597+CassioManuguerra@users.noreply.github.com>
2024-05-08 19:43:53 +0200

    Added DB functions

--- app/src/main/java/com/lastaoutdoor/lasta/data/db/TokenDBRepositoryImpl.kt
+++ app/src/main/java/com/lastaoutdoor/lasta/data/db/TokenDBRepositoryImpl.kt
@@ -1,17 +1,32 @@
 package com.lastaoutdoor.lasta.data.db
 
+import android.content.Context
+import com.google.firebase.Timestamp
+import com.google.firebase.firestore.FirebaseFirestore
 import com.lastaoutdoor.lasta.repository.db.TokenDBRepository
+import kotlinx.coroutines.tasks.await
+import javax.inject.Inject
 
-class TokenDBRepositoryImpl : TokenDBRepository {
-    override fun uploadUserToken(userId: String, token: String) {
+class TokenDBRepositoryImpl @Inject constructor(context: Context, database: FirebaseFirestore) : TokenDBRepository {
+    private val tokenCollection = database.collection("user_fcm_tokens")
 
+    override suspend fun uploadUserToken(userId: String, token: String) {
+        if (token.isEmpty()) return
+        if (userId.isEmpty()) return
+        // Upload user token to database
+        val document = tokenCollection.document(userId)
+        val data = hashMapOf("token" to token, "createdAt" to Timestamp.now())
+        document.set(data).await()
     }
 
-    override fun getUserTokenById(userId: String): String? {
-        TODO("Not yet implemented")
+    override suspend fun getUserTokenById(userId: String): String? {
+        if (userId.isEmpty()) return null
+        // Get user token from database
+        val document = tokenCollection.document(userId).get().await()
+        return document.getString("token")
     }
 
-    override fun getTokensByIds(userIds: List<String>): List<String> {
+    override suspend fun getTokensByIds(userIds: List<String>): List<String> {
         TODO("Not yet implemented")
     }
 }
--- app/src/main/java/com/lastaoutdoor/lasta/repository/db/TokenDBRepository.kt
+++ app/src/main/java/com/lastaoutdoor/lasta/repository/db/TokenDBRepository.kt
@@ -5,17 +5,17 @@ interface TokenDBRepository {
     /**
      * Upload user token to database
      */
-    fun uploadUserToken(userId: String, token: String)
+    suspend fun uploadUserToken(userId: String, token: String)
 
     /**
      * Get user token from database
      */
-    fun getUserTokenById(userId: String): String?
+    suspend fun getUserTokenById(userId: String): String?
 
     /**
      * Get multiple tokens from database for topics
      */
-    fun getTokensByIds(userIds : List<String>): List<String>
+    suspend fun getTokensByIds(userIds : List<String>): List<String>
 
 
 }
