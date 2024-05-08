793264468ea00989c2c0e762f59ab95c646829e8
Andrew Limond <andrew.limond@epfl.ch>
2024-05-08 21:01:40 +0200

    Created API calls to reach the server

---
+++ app/src/main/java/com/lastaoutdoor/lasta/data/api/notifications/FCMApi.kt
@@ -0,0 +1,14 @@
+package com.lastaoutdoor.lasta.data.api.notifications
+
+import com.lastaoutdoor.lasta.models.notifications.SendMessageDto
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface FCMApi {
+
+  @POST("/send")
+  suspend fun sendMessage(@Body body: SendMessageDto)
+
+  @POST("/broadcast")
+  suspend fun broadcast(@Body body: SendMessageDto)
+}
