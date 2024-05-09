4395ec83f70b9da8ce4f91d4b0a412941b77071b
Jérémy Hugentobler <jeremy.hugentobler@epfl.ch>
2024-05-09 13:24:01 +0200

    KTFMT

--- app/src/test/java/com/lastaoutdoor/lasta/data/api/autocomplete/RadarApiResponseTest.kt
+++ app/src/test/java/com/lastaoutdoor/lasta/data/api/autocomplete/RadarApiResponseTest.kt
@@ -2,33 +2,31 @@ package com.lastaoutdoor.lasta.data.api.autocomplete
 
 import com.lastaoutdoor.lasta.models.api.RadarSuggestion
 import org.junit.Assert.*
-
 import org.junit.Before
 import org.junit.Test
 
 class RadarApiResponseTest {
 
-    private lateinit var radarApiResponse: RadarApiResponse
+  private lateinit var radarApiResponse: RadarApiResponse
 
-    @Before
-    fun setUp() {
-        radarApiResponse = RadarApiResponse(emptyList())
-    }
+  @Before
+  fun setUp() {
+    radarApiResponse = RadarApiResponse(emptyList())
+  }
 
-    @Test
-    fun getSuggestions() {
-        assert(radarApiResponse.suggestions.isEmpty())
-        val suggestions = listOf(RadarSuggestion("suggestion", 80.0, 80.0))
-        radarApiResponse = RadarApiResponse(suggestions)
-        assert(radarApiResponse.suggestions.isNotEmpty())
-    }
+  @Test
+  fun getSuggestions() {
+    assert(radarApiResponse.suggestions.isEmpty())
+    val suggestions = listOf(RadarSuggestion("suggestion", 80.0, 80.0))
+    radarApiResponse = RadarApiResponse(suggestions)
+    assert(radarApiResponse.suggestions.isNotEmpty())
+  }
 
-    @Test
-    fun copy() {
-        val suggestions = listOf(RadarSuggestion("suggestion", 80.0, 80.0))
-        radarApiResponse = RadarApiResponse(suggestions)
-        val copy = radarApiResponse.copy()
-        assert(copy == radarApiResponse)
-    }
-
+  @Test
+  fun copy() {
+    val suggestions = listOf(RadarSuggestion("suggestion", 80.0, 80.0))
+    radarApiResponse = RadarApiResponse(suggestions)
+    val copy = radarApiResponse.copy()
+    assert(copy == radarApiResponse)
+  }
 }
--- app/src/test/java/com/lastaoutdoor/lasta/data/api/autocomplete/RadarRepositoryImplTest.kt
+++ app/src/test/java/com/lastaoutdoor/lasta/data/api/autocomplete/RadarRepositoryImplTest.kt
@@ -1,30 +1,18 @@
 package com.lastaoutdoor.lasta.data.api.autocomplete
 
-import com.lastaoutdoor.lasta.BuildConfig
-import com.lastaoutdoor.lasta.data.api.weather.Main
-import com.lastaoutdoor.lasta.data.api.weather.WeatherApiService
-import com.lastaoutdoor.lasta.data.api.weather.WeatherRepositoryImpl
-import com.lastaoutdoor.lasta.data.api.weather.WeatherResponse
-import com.lastaoutdoor.lasta.data.api.weather.Wind
-import com.lastaoutdoor.lasta.repository.api.RadarRepository
-import junit.framework.TestCase
-import kotlinx.coroutines.runBlocking
 import org.junit.Assert.*
-import org.junit.Before
-import org.junit.Test
-import org.mockito.Mock
-import org.mockito.Mockito
 import org.mockito.Mockito.mock
-import org.mockito.MockitoAnnotations
 import retrofit2.Call
 
 class FakeRadarApiServiceTest : RadarApiService {
-    override fun getSuggestions(query: String, layers: String, country: String, limit: Int): Call<RadarApiResponse> {
-        return mock(Call::class.java) as Call<RadarApiResponse>
-    }
-
+  override fun getSuggestions(
+      query: String,
+      layers: String,
+      country: String,
+      limit: Int
+  ): Call<RadarApiResponse> {
+    return mock(Call::class.java) as Call<RadarApiResponse>
+  }
 }
 
-class RadarRepositoryImplTest {
-
-}
+class RadarRepositoryImplTest {}
