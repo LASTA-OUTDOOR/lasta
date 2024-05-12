d6178a876e4500dbfdba97d8f49b5ad4ac15d69c
Andrew Limond <andrew.limond@epfl.ch>
2024-05-12 22:04:48 +0200

    Finished testing osm api

--- app/src/test/java/com/lastaoutdoor/lasta/data/api/osm/ActivityRepositoryImplTest.kt
+++ app/src/test/java/com/lastaoutdoor/lasta/data/api/osm/ActivityRepositoryImplTest.kt
@@ -1,6 +1,8 @@
 package com.lastaoutdoor.lasta.data.api.osm
 
 import com.lastaoutdoor.lasta.models.api.NodeWay
+import com.lastaoutdoor.lasta.models.api.Relation
+import com.lastaoutdoor.lasta.models.api.Tags
 import com.lastaoutdoor.lasta.repository.api.ActivityRepository
 import io.mockk.clearAllMocks
 import io.mockk.every
@@ -75,6 +77,256 @@ class ActivityRepositoryImplTest {
 
     runTest {
       val response = repository.getClimbingPointsInfo(10, 0.0, 0.0)
+      assert(response is com.lastaoutdoor.lasta.utils.Response.Failure)
+    }
+  }
+
+  @Test
+  fun `test getClimbingPointById success`() {
+    val mockCall = mockk<Call<APIResponse<NodeWay>>>()
+
+    every { osmApiService.getClimbingPointById(any()) } returns mockCall
+    every { mockCall.enqueue(any()) } answers {
+      val callback = arg<Callback<APIResponse<NodeWay>>>(0)
+      callback.onResponse(mockCall, Response.success(APIResponse(listOf(NodeWay("node", 0L, null, null, null, Tags())))))
+    }
+
+    runTest {
+      val response = repository.getClimbingPointById(10)
+      assert(response is com.lastaoutdoor.lasta.utils.Response.Success)
+    }
+  }
+
+  @Test
+  fun `test getClimbingPointById failure after reaching endpoint`() {
+    val mockCall = mockk<Call<APIResponse<NodeWay>>>()
+    val res = mockk<Response<APIResponse<NodeWay>>>()
+
+    every { osmApiService.getClimbingPointById(any()) } returns mockCall
+    every { res.isSuccessful } returns false
+    every { mockCall.enqueue(any()) } answers {
+      val callback = arg<Callback<APIResponse<NodeWay>>>(0)
+      callback.onResponse(mockCall, res)
+    }
+
+    runTest {
+      val response = repository.getClimbingPointById(10)
+      assert(response is com.lastaoutdoor.lasta.utils.Response.Failure)
+    }
+  }
+
+  @Test
+  fun `test getClimbingPointById failure`() {
+    val mockCall = mockk<Call<APIResponse<NodeWay>>>()
+
+    every { osmApiService.getClimbingPointById(any()) } returns mockCall
+    every { mockCall.enqueue(any()) } answers {
+      val callback = arg<Callback<APIResponse<NodeWay>>>(0)
+      callback.onFailure(mockCall, Throwable("Error"))
+    }
+
+    runTest {
+      val response = repository.getClimbingPointById(10)
+      assert(response is com.lastaoutdoor.lasta.utils.Response.Failure)
+    }
+  }
+
+  @Test
+  fun `test get HikingRoutesInfo success`() {
+    val mockCall = mockk<Call<APIResponse<Relation>>>()
+
+    every { osmApiService.getHikingRoutesInfo(any()) } returns mockCall
+    every { mockCall.enqueue(any()) } answers {
+      val callback = arg<Callback<APIResponse<Relation>>>(0)
+      callback.onResponse(mockCall, Response.success(APIResponse(listOf())))
+    }
+
+    runTest {
+      val response = repository.getHikingRoutesInfo(10, 0.0, 0.0)
+      assert(response is com.lastaoutdoor.lasta.utils.Response.Success)
+    }
+  }
+
+  @Test
+  fun `test get HikingRoutesInfo failure after reaching endpoint`() {
+    val mockCall = mockk<Call<APIResponse<Relation>>>()
+    val res = mockk<Response<APIResponse<Relation>>>()
+
+    every { osmApiService.getHikingRoutesInfo(any()) } returns mockCall
+    every { res.isSuccessful } returns false
+    every { mockCall.enqueue(any()) } answers {
+      val callback = arg<Callback<APIResponse<Relation>>>(0)
+      callback.onResponse(mockCall, res)
+    }
+
+    runTest {
+      val response = repository.getHikingRoutesInfo(10, 0.0, 0.0)
+      assert(response is com.lastaoutdoor.lasta.utils.Response.Failure)
+    }
+  }
+
+  @Test
+  fun `test get HikingRoutesInfo failure`() {
+    val mockCall = mockk<Call<APIResponse<Relation>>>()
+
+    every { osmApiService.getHikingRoutesInfo(any()) } returns mockCall
+    every { mockCall.enqueue(any()) } answers {
+      val callback = arg<Callback<APIResponse<Relation>>>(0)
+      callback.onFailure(mockCall, Throwable("Error"))
+    }
+
+    runTest {
+      val response = repository.getHikingRoutesInfo(10, 0.0, 0.0)
+      assert(response is com.lastaoutdoor.lasta.utils.Response.Failure)
+    }
+  }
+
+  @Test
+  fun `test getBikingRoutesInfo success`() {
+    val mockCall = mockk<Call<APIResponse<Relation>>>()
+
+    every { osmApiService.getBikingRoutesInfo(any()) } returns mockCall
+    every { mockCall.enqueue(any()) } answers {
+      val callback = arg<Callback<APIResponse<Relation>>>(0)
+      callback.onResponse(mockCall, Response.success(APIResponse(listOf())))
+    }
+
+    runTest {
+      val response = repository.getBikingRoutesInfo(10, 0.0, 0.0)
+      assert(response is com.lastaoutdoor.lasta.utils.Response.Success)
+    }
+  }
+
+  @Test
+  fun `test getBikingRoutesInfo failure after reaching endpoint`() {
+    val mockCall = mockk<Call<APIResponse<Relation>>>()
+    val res = mockk<Response<APIResponse<Relation>>>()
+
+    every { osmApiService.getBikingRoutesInfo(any()) } returns mockCall
+    every { res.isSuccessful } returns false
+    every { mockCall.enqueue(any()) } answers {
+      val callback = arg<Callback<APIResponse<Relation>>>(0)
+      callback.onResponse(mockCall, res)
+    }
+
+    runTest {
+      val response = repository.getBikingRoutesInfo(10, 0.0, 0.0)
+      assert(response is com.lastaoutdoor.lasta.utils.Response.Failure)
+    }
+  }
+
+  @Test
+  fun `test getBikingRoutesInfo failure`() {
+    val mockCall = mockk<Call<APIResponse<Relation>>>()
+
+    every { osmApiService.getBikingRoutesInfo(any()) } returns mockCall
+    every { mockCall.enqueue(any()) } answers {
+      val callback = arg<Callback<APIResponse<Relation>>>(0)
+      callback.onFailure(mockCall, Throwable("Error"))
+    }
+
+    runTest {
+      val response = repository.getBikingRoutesInfo(10, 0.0, 0.0)
+      assert(response is com.lastaoutdoor.lasta.utils.Response.Failure)
+    }
+  }
+
+  @Test
+  fun `test getHikingRouteById success`() {
+    val mockCall = mockk<Call<APIResponse<Relation>>>()
+
+    every { osmApiService.getHikingRouteById(any()) } returns mockCall
+    every { mockCall.enqueue(any()) } answers {
+      val callback = arg<Callback<APIResponse<Relation>>>(0)
+      callback.onResponse(mockCall, Response.success(APIResponse(listOf(Relation("relation", 0L, Tags(), null)))))
+    }
+
+    runTest {
+      val response = repository.getHikingRouteById(10)
+      assert(response is com.lastaoutdoor.lasta.utils.Response.Success)
+    }
+  }
+
+  @Test
+  fun `test getHikingRouteById failure after reaching endpoint`() {
+    val mockCall = mockk<Call<APIResponse<Relation>>>()
+    val res = mockk<Response<APIResponse<Relation>>>()
+
+    every { osmApiService.getHikingRouteById(any()) } returns mockCall
+    every { res.isSuccessful } returns false
+    every { mockCall.enqueue(any()) } answers {
+      val callback = arg<Callback<APIResponse<Relation>>>(0)
+      callback.onResponse(mockCall, res)
+    }
+
+    runTest {
+      val response = repository.getHikingRouteById(10)
+      assert(response is com.lastaoutdoor.lasta.utils.Response.Failure)
+    }
+  }
+
+  @Test
+  fun `test getHikingRouteById failure`() {
+    val mockCall = mockk<Call<APIResponse<Relation>>>()
+
+    every { osmApiService.getHikingRouteById(any()) } returns mockCall
+    every { mockCall.enqueue(any()) } answers {
+      val callback = arg<Callback<APIResponse<Relation>>>(0)
+      callback.onFailure(mockCall, Throwable("Error"))
+    }
+
+    runTest {
+      val response = repository.getHikingRouteById(10)
+      assert(response is com.lastaoutdoor.lasta.utils.Response.Failure)
+    }
+  }
+
+  @Test
+  fun `test getBikingRouteById success`() {
+    val mockCall = mockk<Call<APIResponse<Relation>>>()
+
+    every { osmApiService.getBikingRouteById(any()) } returns mockCall
+    every { mockCall.enqueue(any()) } answers {
+      val callback = arg<Callback<APIResponse<Relation>>>(0)
+      callback.onResponse(mockCall, Response.success(APIResponse(listOf(Relation("relation", 0L, Tags(), null)))))
+    }
+
+    runTest {
+      val response = repository.getBikingRouteById(10)
+      assert(response is com.lastaoutdoor.lasta.utils.Response.Success)
+    }
+  }
+
+  @Test
+  fun `test getBikingRouteById failure after reaching endpoint`() {
+    val mockCall = mockk<Call<APIResponse<Relation>>>()
+    val res = mockk<Response<APIResponse<Relation>>>()
+
+    every { osmApiService.getBikingRouteById(any()) } returns mockCall
+    every { res.isSuccessful } returns false
+    every { mockCall.enqueue(any()) } answers {
+      val callback = arg<Callback<APIResponse<Relation>>>(0)
+      callback.onResponse(mockCall, res)
+    }
+
+    runTest {
+      val response = repository.getBikingRouteById(10)
+      assert(response is com.lastaoutdoor.lasta.utils.Response.Failure)
+    }
+  }
+
+  @Test
+  fun `test getBikingRouteById failure`() {
+    val mockCall = mockk<Call<APIResponse<Relation>>>()
+
+    every { osmApiService.getBikingRouteById(any()) } returns mockCall
+    every { mockCall.enqueue(any()) } answers {
+      val callback = arg<Callback<APIResponse<Relation>>>(0)
+      callback.onFailure(mockCall, Throwable("Error"))
+    }
+
+    runTest {
+      val response = repository.getBikingRouteById(10)
       assert(response is com.lastaoutdoor.lasta.utils.Response.Failure)
     }
   }
