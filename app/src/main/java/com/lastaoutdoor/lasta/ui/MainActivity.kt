36602e91ee226273927815cf50e94c7be0ac4889
Emile Cornamusaz <emile.cornamusaz@epfl.ch>
2024-05-13 21:38:10 +0200

    reverted small mistake

--- app/src/main/java/com/lastaoutdoor/lasta/ui/MainActivity.kt
+++ app/src/main/java/com/lastaoutdoor/lasta/ui/MainActivity.kt
@@ -4,7 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.lastaoutdoor.lasta.ui.components.StepCountDisplay
+import com.lastaoutdoor.lasta.ui.navigation.AppNavGraph
+import com.lastaoutdoor.lasta.ui.theme.LastaTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -12,9 +13,6 @@ class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     enableEdgeToEdge()
     super.onCreate(savedInstanceState)
-    setContent {
-      // LastaTheme { AppNavGraph() }
-      StepCountDisplay()
-    }
+    setContent { LastaTheme { AppNavGraph() } }
   }
 }
