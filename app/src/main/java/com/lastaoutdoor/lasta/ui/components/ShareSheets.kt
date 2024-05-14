d5f7b008961c9af9a6b07c463d5fe9442c54682b
Jérémy Hugentobler <jeremy.hugentobler@epfl.ch>
2024-05-14 15:49:33 +0200

    Changes requested by Emile and better commenting

--- app/src/main/java/com/lastaoutdoor/lasta/ui/components/ShareSheets.kt
+++ app/src/main/java/com/lastaoutdoor/lasta/ui/components/ShareSheets.kt
@@ -5,19 +5,37 @@ import android.content.Intent
 import com.lastaoutdoor.lasta.R
 import com.lastaoutdoor.lasta.models.activity.Activity
 
-// Opens the android share sheet, will contain a description and a link to the activity -> will be
-// used to share activities
-fun shareActivity(activity: Activity, context: Context) {
-
+/**
+ * Share a text to other apps (share sheet)
+ *
+ * @param content the text to share
+ * @param context the context of the app
+ * @return void
+ */
+private fun shareText(content: String, context: Context) {
   val sendIntent: Intent =
       Intent().apply {
         action = Intent.ACTION_SEND
-        putExtra(
-            Intent.EXTRA_TEXT,
-            context.getString(R.string.share_body) +
-                "\n${activity.name} \nhttps://lasta.jerem.ch/activities/${activity.activityId}")
+        putExtra(Intent.EXTRA_TEXT, content)
         type = "text/plain"
       }
-
   context.startActivity(Intent.createChooser(sendIntent, context.getString(R.string.share_title)))
+}
+
+/**
+ * Starts a share sheet to share an activity
+ *
+ * @param activity the activity to share
+ * @param context the context of the app
+ * @return void
+ */
+fun shareActivity(activity: Activity, context: Context) {
+
+  val textFromActivity =
+      context.getString(R.string.share_body) +
+          "\n${activity.name} \n" +
+          context.getString(R.string.share_base_url) +
+          activity.activityId
+
+  shareText(textFromActivity, context)
 }
