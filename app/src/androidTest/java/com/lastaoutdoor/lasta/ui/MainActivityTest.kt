d009f66076fbfc3515714f89cca53fd8d869ab50
npfrei <126064815+npfrei@users.noreply.github.com>
2024-05-15 15:38:11 +0200

    final test

--- app/src/androidTest/java/com/lastaoutdoor/lasta/ui/MainActivityTest.kt
+++ app/src/androidTest/java/com/lastaoutdoor/lasta/ui/MainActivityTest.kt
@@ -1,9 +1,11 @@
 package com.lastaoutdoor.lasta.ui
 
+import androidx.lifecycle.Lifecycle
 import androidx.test.core.app.ActivityScenario.launch
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import org.junit.Assert.assertEquals
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -15,8 +17,11 @@ class MainActivityTest {
   @get:Rule(order = 0) val hiltRule = HiltAndroidRule(this)
 
   @Test
-  fun testMainActivity() {
+  fun testMainActivityIsLaunched() {
 
-    launch(MainActivity::class.java).use { it.recreate() }
+    launch(MainActivity::class.java).use {
+      it.recreate()
+      assertEquals(it.state, Lifecycle.State.RESUMED)
+    }
   }
 }
