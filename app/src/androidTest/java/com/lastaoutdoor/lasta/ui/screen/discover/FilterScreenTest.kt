759ad18bfea9459012084f53cc93ff7300a0bc54
Emile Cornamusaz <emile.cornamusaz@epfl.ch>
2024-05-15 21:33:27 +0200

    testing

--- app/src/androidTest/java/com/lastaoutdoor/lasta/ui/screen/discover/FilterScreenTest.kt
+++ app/src/androidTest/java/com/lastaoutdoor/lasta/ui/screen/discover/FilterScreenTest.kt
@@ -66,7 +66,11 @@ class FilterScreenTest {
 
     composeRule.onNodeWithTag("EraseButton").performClick()
 
+    composeRule.onNodeWithTag("filterScreen").assertIsDisplayed()
+
     composeRule.onNodeWithTag("applyFilterOptionsButton").performClick()
+
+    composeRule.onNodeWithTag("filterScreen").assertIsDisplayed()
   }
 
   @Test
