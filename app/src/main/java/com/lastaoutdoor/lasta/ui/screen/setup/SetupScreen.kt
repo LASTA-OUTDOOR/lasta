40734d71ee19c846fd6df911e793d9e8b3b2e854
Cassio Manuguerra <91049597+CassioManuguerra@users.noreply.github.com>
2024-05-16 10:39:44 +0200

    this is nightmarish

--- app/src/main/java/com/lastaoutdoor/lasta/ui/screen/setup/SetupScreen.kt
+++ app/src/main/java/com/lastaoutdoor/lasta/ui/screen/setup/SetupScreen.kt
@@ -2,13 +2,13 @@ package com.lastaoutdoor.lasta.ui.screen.setup
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -73,46 +73,38 @@ fun SetupScreen(
         properties = DialogProperties(dismissOnBackPress = false, dismissOnClickOutside = false))
   }
 
-  LazyColumn(
+  Column(
       modifier =
           Modifier.fillMaxSize()
               .padding(horizontal = 16.dp, vertical = 8.dp)
               .testTag("setupScreen"),
       verticalArrangement = Arrangement.SpaceEvenly,
       horizontalAlignment = Alignment.CenterHorizontally) {
-        item {
-          MediumTopAppBar(
-              title = {
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.Center) {
-                      TitleComponent(setUpOrSetting = true)
-                    }
-              })
-        }
+        MediumTopAppBar(
+            title = {
+              Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+                TitleComponent(setUpOrSetting = true)
+              }
+            })
 
-        item {
-          SettingsComponent(
-              language = language,
-              prefActivity = prefActivity,
-              levels = levels,
-              updateLanguage = updateLanguage,
-              updatePrefActivity = updatePrefActivity,
-              updateClimbingLevel = updateClimbingLevel,
-              updateHikingLevel = updateHikingLevel,
-              updateBikingLevel = updateBikingLevel)
-        }
+        SettingsComponent(
+            language = language,
+            prefActivity = prefActivity,
+            levels = levels,
+            updateLanguage = updateLanguage,
+            updatePrefActivity = updatePrefActivity,
+            updateClimbingLevel = updateClimbingLevel,
+            updateHikingLevel = updateHikingLevel,
+            updateBikingLevel = updateBikingLevel)
 
-        item { Spacer(modifier = Modifier.fillMaxHeight(0.025f)) }
+        Spacer(modifier = Modifier.fillMaxHeight(0.025f))
 
-        item {
-          Row(verticalAlignment = Alignment.CenterVertically) {
-            Button(
-                modifier = Modifier.testTag("setupFinishButton"),
-                onClick = { showSubmitDialog.value = true },
-            ) {
-              Text(text = LocalContext.current.getString(R.string.finish))
-            }
+        Row(verticalAlignment = Alignment.CenterVertically) {
+          Button(
+              modifier = Modifier.testTag("setupFinishButton"),
+              onClick = { showSubmitDialog.value = true },
+          ) {
+            Text(text = LocalContext.current.getString(R.string.finish))
           }
         }
       }
