3bc73118d0ac9ec316c863df5f40b8aac0e0b853
Emile Cornamusaz <emile.cornamusaz@epfl.ch>
2024-05-27 17:36:47 +0200

    added a disclaimer for empty favorites list

--- app/src/main/java/com/lastaoutdoor/lasta/ui/screen/favorites/FavoritesScreen.kt
+++ app/src/main/java/com/lastaoutdoor/lasta/ui/screen/favorites/FavoritesScreen.kt
@@ -1,8 +1,10 @@
 package com.lastaoutdoor.lasta.ui.screen.favorites
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -15,7 +17,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.google.android.gms.maps.model.LatLng
 import com.lastaoutdoor.lasta.R
 import com.lastaoutdoor.lasta.models.activity.Activity
@@ -46,7 +52,7 @@ fun FavoritesScreen(
     if (isLoading) {
       LoadingAnim(width = 35, tag = "LoadingBarFavorites")
     } else if (favorites.isEmpty()) {
-      /*TODO*/
+      EmptyFavoritesList()
     } else {
       LazyColumn {
         item {
@@ -63,4 +69,19 @@ fun FavoritesScreen(
       }
     }
   }
+}
+
+@Composable
+fun EmptyFavoritesList() {
+  Column(
+      modifier = Modifier.fillMaxSize().testTag("FriendMissing"),
+      verticalArrangement = Arrangement.Center,
+      horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(
+            text = LocalContext.current.getString(R.string.no_fav),
+            style =
+                TextStyle(
+                    fontWeight = FontWeight.Normal, fontSize = 20.sp, textAlign = TextAlign.Center),
+            modifier = Modifier.padding(15.dp))
+      }
 }
