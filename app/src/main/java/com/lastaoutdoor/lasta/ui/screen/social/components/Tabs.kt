ebe754faaa387dcdf1c9fdcf650351829c5a32cb
Jérémy Hugentobler <jeremy.hugentobler@epfl.ch>
2024-05-29 18:55:49 +0200

    Pager is in place, but there is a glitch with icons

--- app/src/main/java/com/lastaoutdoor/lasta/ui/screen/social/components/Tabs.kt
+++ app/src/main/java/com/lastaoutdoor/lasta/ui/screen/social/components/Tabs.kt
@@ -1,6 +1,11 @@
 package com.lastaoutdoor.lasta.ui.screen.social.components
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Email
@@ -9,10 +14,12 @@ import androidx.compose.material3.PrimaryTabRow
 import androidx.compose.material3.Tab
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
@@ -24,7 +31,8 @@ import com.lastaoutdoor.lasta.models.social.FriendsActivities
 import com.lastaoutdoor.lasta.models.user.UserModel
 import com.lastaoutdoor.lasta.utils.ConnectionState
 
-@OptIn(ExperimentalMaterial3Api::class)
+
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
 @Composable
 fun TabMenu(
     isConnected: ConnectionState,
@@ -52,58 +60,80 @@ fun TabMenu(
     refreshFriendsActivities: () -> Unit
 ) {
 
-  var state by remember { mutableIntStateOf(0) }
-  val titles =
-      listOf(
-          LocalContext.current.getString(R.string.feed),
-          LocalContext.current.getString(R.string.friends),
-          LocalContext.current.getString(R.string.message))
+  val tabs =
+    listOf(
+        LocalContext.current.getString(R.string.feed),
+        LocalContext.current.getString(R.string.friends),
+        LocalContext.current.getString(R.string.message))
+
+  //states for the tabs
+  var selectedTab by remember { mutableIntStateOf(0) }
+  val pagerState = rememberPagerState {
+      tabs.size
+  }
+
+  //Listen for tab changes (when clicking on a tab)
+  LaunchedEffect(selectedTab){
+      pagerState.scrollToPage(selectedTab)
+  }
+
+  //Listen for swipe changes (when swiping between tabs)
+  LaunchedEffect(pagerState.currentPage){
+      selectedTab = pagerState.currentPage
+  }
 
   Column {
-    PrimaryTabRow(selectedTabIndex = state) {
-      titles.forEachIndexed { index, title ->
+    PrimaryTabRow(selectedTabIndex = selectedTab) {
+
+      //iterate through the tabs list and create a Tab composable for each tab
+      tabs.forEachIndexed { index, title ->
         Tab(
-            selected = state == index,
-            onClick = { state = index },
+            selected = selectedTab == index,
+            onClick = { selectedTab = index }, //update selected tab index
             modifier = Modifier.testTag("TabNumber$index"),
             text = { Text(text = title, maxLines = 2, overflow = TextOverflow.Ellipsis) })
       }
     }
-  }
 
-  // use the state variable to choose which composable to display
-  when (state) {
-    0 -> {
-      hideTopButton()
-      refreshFriendsActivities()
-      FriendsActivityList(isConnected, latestFriendActivities)
+    // Pager to be able to swipe between the tabs
+    HorizontalPager(state = pagerState, modifier = Modifier.fillMaxHeight().weight(1f), verticalAlignment = Alignment.Top) {
+        index ->
+        when (index) {
+          0 -> {
+              hideTopButton()
+              refreshFriendsActivities()
+              FriendsActivityList(isConnected, latestFriendActivities)
+          }
+          1 -> {
+              showTopButton(Icons.Filled.Add) { displayAddFriendDialog() }
+              FriendsList(
+                  isConnected,
+                  friends,
+                  addFriendDialog,
+                  friendRequestFeedback,
+                  friendSuggestions,
+                  clearFriendRequestFeedback,
+                  hideAddFriendDialog,
+                  fetchFriendsSuggestions,
+                  requestFriend,
+                  refreshFriends,
+                  navigateToFriendProfile)
+          }
+          2 -> {
+              showTopButton(Icons.Filled.Email) { displayFriendPicker() }
+              MessageList(
+                  isConnected,
+                  messages,
+                  refreshMessages,
+                  friends,
+                  hideFriendPicker,
+                  navigateToConversation,
+                  isDisplayedFriendPicker,
+                  changeDisplayFriendPicker)
+          }
     }
-    1 -> {
-      showTopButton(Icons.Filled.Add) { displayAddFriendDialog() }
-      FriendsList(
-          isConnected,
-          friends,
-          addFriendDialog,
-          friendRequestFeedback,
-          friendSuggestions,
-          clearFriendRequestFeedback,
-          hideAddFriendDialog,
-          fetchFriendsSuggestions,
-          requestFriend,
-          refreshFriends,
-          navigateToFriendProfile)
-    }
-    2 -> {
-      showTopButton(Icons.Filled.Email) { displayFriendPicker() }
-      MessageList(
-          isConnected,
-          messages,
-          refreshMessages,
-          friends,
-          hideFriendPicker,
-          navigateToConversation,
-          isDisplayedFriendPicker,
-          changeDisplayFriendPicker)
-    }
+
+  }
   }
 }
+
