bc657cae1b161a003828ed6a748279c525243e1b
Andrew Limond <andrew.limond@epfl.ch>
2024-05-29 13:58:12 +0200

    Separated everything into different component files

--- app/src/main/java/com/lastaoutdoor/lasta/ui/screen/tracking/TrackingScreen.kt
+++ app/src/main/java/com/lastaoutdoor/lasta/ui/screen/tracking/TrackingScreen.kt
@@ -4,33 +4,22 @@ import android.hardware.Sensor
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.material.Button
-import androidx.compose.material.ButtonDefaults
-import androidx.compose.material.Text
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Color.Companion.Blue
-import androidx.compose.ui.graphics.Color.Companion.Red
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.testTag
-import androidx.compose.ui.unit.dp
 import com.google.android.gms.location.LocationCallback
 import com.google.android.gms.maps.model.LatLng
 import com.lastaoutdoor.lasta.services.StopwatchService
 import com.lastaoutdoor.lasta.services.StopwatchServiceHelper
 import com.lastaoutdoor.lasta.services.StopwatchState
+import com.lastaoutdoor.lasta.ui.screen.tracking.components.TrackingButtons
 import com.lastaoutdoor.lasta.ui.screen.tracking.components.TrackingDispatchers
 import com.lastaoutdoor.lasta.ui.screen.tracking.components.TrackingInfo
 import com.lastaoutdoor.lasta.ui.screen.tracking.components.TrackingMap
@@ -72,42 +61,21 @@ fun TrackingScreen(
               trackingState.positions.map { position -> LatLng(position.lat, position.lon) },
               Modifier.weight(0.4f))
           TrackingInfo(Modifier.weight(0.5f), trackingState, hours, minutes, seconds)
-          Row(
-              modifier = Modifier.weight(0.1f).fillMaxSize(),
-              verticalAlignment = Alignment.Top,
-              horizontalArrangement = Arrangement.Center) {
-                Button(
-                    modifier = Modifier.weight(1f).fillMaxHeight(0.8f),
-                    onClick = {
-                      StopwatchServiceHelper.triggerForegroundService(
-                          context = context,
-                          action =
-                              if (currentState == StopwatchState.Started) ACTION_SERVICE_STOP
-                              else ACTION_SERVICE_START)
-                    },
-                    colors =
-                        ButtonDefaults.buttonColors(
-                            backgroundColor =
-                                if (currentState == StopwatchState.Started) Red else Blue,
-                            contentColor = Color.White)) {
-                      Text(
-                          text =
-                              if (currentState == StopwatchState.Started) "Stop"
-                              else if ((currentState == StopwatchState.Stopped)) "Resume"
-                              else "Start")
-                    }
-                Spacer(modifier = Modifier.width(30.dp))
-                Button(
-                    modifier = Modifier.weight(1f).fillMaxHeight(0.8f),
-                    onClick = {
-                      StopwatchServiceHelper.triggerForegroundService(
-                          context = context, action = ACTION_SERVICE_CANCEL)
-                    },
-                    enabled = seconds != "00" && currentState != StopwatchState.Started,
-                    colors = ButtonDefaults.buttonColors(Color.Gray)) {
-                      Text(text = "Cancel")
-                    }
-              }
+          TrackingButtons(
+              currentState = currentState,
+              cancelEnabled = seconds != "00" && currentState != StopwatchState.Started,
+              onClickStart = {
+                StopwatchServiceHelper.triggerForegroundService(
+                    context = context,
+                    action =
+                        if (currentState == StopwatchState.Started) ACTION_SERVICE_STOP
+                        else ACTION_SERVICE_START)
+              },
+              onClickCancel = {
+                StopwatchServiceHelper.triggerForegroundService(
+                    context = context, action = ACTION_SERVICE_CANCEL)
+              },
+              Modifier.weight(0.1f))
         }
   }
 }
---
+++ app/src/main/java/com/lastaoutdoor/lasta/ui/screen/tracking/components/TrackingButtons.kt
@@ -0,0 +1,56 @@
+package com.lastaoutdoor.lasta.ui.screen.tracking.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.lastaoutdoor.lasta.services.StopwatchState
+import com.lastaoutdoor.lasta.ui.theme.RedDifficulty
+
+@Composable
+fun TrackingButtons(
+    currentState: StopwatchState,
+    cancelEnabled: Boolean,
+    onClickStart: () -> Unit,
+    onClickCancel: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+  Row(
+      modifier = modifier.fillMaxSize().padding(horizontal = 16.dp),
+      verticalAlignment = Alignment.Top,
+      horizontalArrangement = Arrangement.SpaceEvenly) {
+        Button(
+            modifier = Modifier.fillMaxHeight(0.8f).weight(0.45f),
+            onClick = onClickStart,
+            colors =
+                ButtonDefaults.buttonColors(
+                    containerColor =
+                        if (currentState == StopwatchState.Started) RedDifficulty
+                        else MaterialTheme.colorScheme.primary,
+                    contentColor = Color.White)) {
+              Text(
+                  text =
+                      if (currentState == StopwatchState.Started) "Stop"
+                      else if ((currentState == StopwatchState.Stopped)) "Resume" else "Start")
+            }
+        Spacer(modifier = Modifier.weight(0.1f))
+        Button(
+            modifier = Modifier.fillMaxHeight(0.8f).weight(0.45f),
+            onClick = onClickCancel,
+            enabled = cancelEnabled,
+            colors = ButtonDefaults.buttonColors(Color.Gray)) {
+              Text(text = "Cancel")
+            }
+      }
+}
